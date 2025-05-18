3bf5f525b2d33e8b4024eddcc596bf99aa132fff
Reza <reza@ies-italia.it>
2025-05-18 19:06:40 +0200

    Add media association to CollectionItems and restore collections
    Re-introduced the collections relationship in the Media struct and added a Media association in CollectionItems. These changes enhance relational mapping and improve data handling for media collections.

--- models.go
+++ models.go
@@ -6,26 +6,26 @@ import (
 )
 
 type Media struct {
-	MediaID        int64      `gorm:"column:media_id;primaryKey;autoIncrement" json:"media_id"`
-	ExternalID     string     `gorm:"column:external_id;size:64;index" json:"external_id"`
-	ExternalStatus string     `gorm:"column:state;size:64" json:"state"`
-	Title          string     `gorm:"column:title;size:255" json:"title"`
-	Filename       string     `gorm:"column:filename;size:255" json:"filename"`
-	Path           string     `gorm:"column:path;size:255" json:"path"`
-	Description    string     `gorm:"column:description;size:512" json:"description"`
-	Thumbnail      string     `gorm:"column:thumbnail;size:255" json:"thumbnail"`
-	Preview        string     `gorm:"column:preview;size:255" json:"preview"`
-	Type           string     `gorm:"column:type;type:enum('image','audio','video','document')" json:"type"`
-	Mimetype       string     `gorm:"column:mimetype;size:32" json:"mimetype"`
-	Duration       int64      `gorm:"column:duration" json:"duration"`
-	ScreenSize     string     `gorm:"column:screen_size;size:16" json:"screen_size"`
-	AspectRatio    string     `gorm:"column:aspect_ratio;size:16" json:"aspect_ratio"`
-	FileSize       int64      `gorm:"column:file_size" json:"file_size"`
-	Status         string     `gorm:"column:status;type:enum('uploading','processing','ready','failed');index" json:"status"`
-	Progress       float64    `gorm:"column:progress" json:"progress"`
-	Error          string     `gorm:"column:error;size:255" json:"error"`
-	MetaData       []MetaData `gorm:"foreignKey:MediaID;references:MediaID" json:"metadata"`
-	//Collections    []Collection `gorm:"many2many:media_collection_items;joinTable:media_collection_items;foreignKey:CollectionID;references:CollectionID" json:""`
+	MediaID        int64        `gorm:"column:media_id;primaryKey;autoIncrement" json:"media_id"`
+	ExternalID     string       `gorm:"column:external_id;size:64;index" json:"external_id"`
+	ExternalStatus string       `gorm:"column:state;size:64" json:"state"`
+	Title          string       `gorm:"column:title;size:255" json:"title"`
+	Filename       string       `gorm:"column:filename;size:255" json:"filename"`
+	Path           string       `gorm:"column:path;size:255" json:"path"`
+	Description    string       `gorm:"column:description;size:512" json:"description"`
+	Thumbnail      string       `gorm:"column:thumbnail;size:255" json:"thumbnail"`
+	Preview        string       `gorm:"column:preview;size:255" json:"preview"`
+	Type           string       `gorm:"column:type;type:enum('image','audio','video','document')" json:"type"`
+	Mimetype       string       `gorm:"column:mimetype;size:32" json:"mimetype"`
+	Duration       int64        `gorm:"column:duration" json:"duration"`
+	ScreenSize     string       `gorm:"column:screen_size;size:16" json:"screen_size"`
+	AspectRatio    string       `gorm:"column:aspect_ratio;size:16" json:"aspect_ratio"`
+	FileSize       int64        `gorm:"column:file_size" json:"file_size"`
+	Status         string       `gorm:"column:status;type:enum('uploading','processing','ready','failed');index" json:"status"`
+	Progress       float64      `gorm:"column:progress" json:"progress"`
+	Error          string       `gorm:"column:error;size:255" json:"error"`
+	MetaData       []MetaData   `gorm:"foreignKey:MediaID;references:MediaID" json:"metadata"`
+	Collections    []Collection `gorm:"many2many:media_collection_items;joinForeignKey:MediaID;joinReferences:CollectionID" json:"collections"`
 	types.CreatedAt
 	types.UpdatedAt
 	types.SoftDelete
@@ -51,9 +51,10 @@ func (Collection) TableName() string {
 }
 
 type CollectionItems struct {
-	CollectionID int64 `gorm:"column:collection_id;index;fk:media_collection" json:"collection_id"`
-	MediaID      int64 `gorm:"column:media_id;index;fk:media_collection" json:"media_id"`
-	VisualOrder  int   `gorm:"column:visual_order" json:"visual_order"`
+	CollectionID int64  `gorm:"column:collection_id;index;fk:media_collection" json:"collection_id"`
+	MediaID      int64  `gorm:"column:media_id;index;fk:media_collection" json:"media_id"`
+	VisualOrder  int    `gorm:"column:visual_order" json:"visual_order"`
+	Media        *Media `gorm:"foreignKey:MediaID;references:MediaID" json:"media,omitempty"`
 	restify.API
 }
 
